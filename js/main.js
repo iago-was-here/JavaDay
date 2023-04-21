9036c48add77edb26b49249184b2ebef08805ed6
Rafael Rarvs <rafael.rarvs@gmail.com>
2023-04-21 13:39:59 -0300

    update: style

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(function () {
     $('.carousel').carousel({
-        interval: 5000,
+        interval: 1000,
         wrap: true,
     });
 })
+
+$(".nav .nav-link").on("click", function(){
+    $(".nav").find(".active").removeClass("active");
+    $(this).addClass("active");
+ });
